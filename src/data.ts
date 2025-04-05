fc11c531e8b4ae785864f9f1abdb871c3971a8a3
darkyzhou <darkyoooooo@gmail.com>
2025-04-05 12:22:27 +0800

    fix: no period for category introductions

--- src/data.ts
+++ src/data.ts
@@ -34,26 +34,26 @@ export const BLOG_CATEGORIES: Record<string, BlogCategory> = {
   'thoughts': {
     slug: 'thoughts',
     title: '随笔',
-    excerpt: '主要是一些和编程无关的思考和分享，作为本博客仅有的一小部分的「人文」色彩。',
+    excerpt: '主要是一些和编程无关的思考和分享，作为本博客仅有的一小部分的「人文」色彩',
   },
   'linux': {
     slug: 'linux',
     title: 'Linux 研究日志',
-    excerpt: '分享我在 Linux 内核和各类发行版上的研究与实践，我目前使用的发行版主要是 NixOS 和 AOSC。一些和龙架构（LoongArch）有关的工作也会记录在这里。',
+    excerpt: '分享我在 Linux 内核和各类发行版上的研究与实践，我目前使用的发行版主要是 NixOS 和 AOSC。一些和龙架构（LoongArch）有关的工作也会记录在这里',
   },
   'web-frontend': {
     slug: 'web-frontend',
     title: '前端修炼手册',
-    excerpt: '主要包含前端开发相关的技术文章，包括但不限于 TypeScript、JavaScript 等。',
+    excerpt: '主要包含前端开发相关的技术文章，包括但不限于 TypeScript、JavaScript 等',
   },
   'programming-miscellaneous': {
     slug: 'programming-miscellaneous',
     title: '编程杂谈',
-    excerpt: '汇总我在编程中遇到的一些问题和解决方案，也包含一些编程相关的思考。',
+    excerpt: '汇总我在编程中遇到的一些问题和解决方案，也包含一些编程相关的思考',
   },
   'cloud-native': {
     slug: 'cloud-native',
     title: '云原生',
-    excerpt: '记录我在云原生领域的学习与实践，包括但不限于 Kubernetes、Docker 等。',
+    excerpt: '记录我在云原生领域的学习与实践，包括但不限于 Kubernetes、Docker 等',
   },
 }
