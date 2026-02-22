64e3f06e06a6226b7b931ba5e112ee800490afe8
darkyzhou <me@zqy.io>
2026-02-22 18:35:40 +0800

    feat: enhance article content and update layout for better readability

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -7,7 +7,7 @@ export type ArticleListProps = PropsWithChildren<{
 
 export function ArticleList({ children, size }: ArticleListProps) {
   return (
-    <div className={clsx('col-span-full grid auto-rows-[1fr] gap-0 md:-mr-12 lg:-mr-16 xl:-mr-32', {
+    <div className={clsx('col-span-full grid auto-rows-[1fr] gap-0', {
       'grid-rows-[repeat(auto-fill,minmax(128px,1fr))]': size === 'small',
       'grid-rows-[repeat(auto-fill,minmax(168px,1fr))]': size === 'large',
     })}
